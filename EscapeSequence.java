4f66831b6a879fae3b6fb4430251debf31fa89fe
123biswash <biswash.adhikari@hotmail.com>
2017-05-26 10:23:04 -0400

    first commit

---
+++ EscapeSequence.java
@@ -0,0 +1,9 @@
+package EscapeSequence;
+
+public class EscapeSequence {
+
+	public static void main(String[] args) {
+		System.out.print("*********\n\n*\t*\n\n*\t*\n\n*\t*\n\n*\t*\n\n*********\n\n\n\n");
+		}
+
+}
